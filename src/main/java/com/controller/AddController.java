21dbd992f0b9936bc2464be911156bcb64cbf8be
swathigajji <swathigajji1234@gmail.com>
2020-05-27 17:00:15 +0530

    mvc

---
+++ src/main/java/com/controller/AddController.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.service.AddService;
+
+@Controller
+public class AddController {
+
+	@RequestMapping("/add")
+	public ModelAndView add(HttpServletRequest request,HttpServletResponse res) {
+		System.out.println("Addcontroller");
+		
+		int i=Integer.parseInt(request.getParameter("t1"));
+		int j=Integer.parseInt(request.getParameter("t2"));
+		
+		
+		AddService service= new AddService();
+		int k=service.add(i, j);
+		
+		ModelAndView mv= new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result",k);
+	
+		return mv;
+	}
+}
---
+++ target/MVC/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="add">
+<input type="text" name="name"/>
+<input type="text" name="password"/>
+<input type="submit"/>
+
+</form>
+</body>
+</html>
